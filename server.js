ef9dc357e7e5d566cdf6f52da85a49be2b9a8ab7
wxyzcctv <2846629226@qq.com>
2020-12-30 07:50:07 +0800

    第一次提交

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var url = require('url');
+
+function start(route, handle) {
+    function onRequest(req, res) {
+        var pathname = url.parse(req.url).pathname;
+        console.log("Request for " + pathname + " recevied");
+
+        route(handle, pathname);
+
+        res.writeHead(200, { "Content-Type": "text/plain" })
+        res.write("Hello World!");
+        res.end()
+    }
+
+    http.createServer(onRequest).listen(8080)
+    console.log('请在浏览器中打开：http://localhost:8080')
+}
+
+exports.start = start
